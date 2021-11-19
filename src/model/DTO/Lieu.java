9e030dee6d215cb15eee6b5177a44c21d5fe98d8
delrazedd <arthurfrindel@gmail.com>
2021-11-19 17:04:22 +0100

    CHANGEMENTS

---
+++ src/model/DTO/Lieu.java
@@ -0,0 +1,70 @@
+package model.DTO;
+
+public class Lieu {
+
+    private int idVille;
+
+    private int idLieu;
+    private String libelleLieu;
+    private String adresseLieu;
+
+    private boolean annulationGratuite;
+    private int nbEtoiles;
+    private String descriptif;
+
+    public int getIdVille() {
+        return idVille;
+    }
+
+    public void setIdVille(int idVille) {
+        this.idVille = idVille;
+    }
+
+    public int getIdLieu() {
+        return idLieu;
+    }
+
+    public void setIdLieu(int idLieu) {
+        this.idLieu = idLieu;
+    }
+
+    public String getLibelleLieu() {
+        return libelleLieu;
+    }
+
+    public void setLibelleLieu(String libelleLieu) {
+        this.libelleLieu = libelleLieu;
+    }
+
+    public String getAdresseLieu() {
+        return adresseLieu;
+    }
+
+    public void setAdresseLieu(String adresseLieu) {
+        this.adresseLieu = adresseLieu;
+    }
+
+    public boolean isAnnulationGratuite() {
+        return annulationGratuite;
+    }
+
+    public void setAnnulationGratuite(boolean annulationGratuite) {
+        this.annulationGratuite = annulationGratuite;
+    }
+
+    public int getNbEtoiles() {
+        return nbEtoiles;
+    }
+
+    public void setNbEtoiles(int nbEtoiles) {
+        this.nbEtoiles = nbEtoiles;
+    }
+
+    public String getDescriptif() {
+        return descriptif;
+    }
+
+    public void setDescriptif(String descriptif) {
+        this.descriptif = descriptif;
+    }
+}
---
+++ src/model/DTO/Salle.java
@@ -0,0 +1,14 @@
+package model.DTO;
+
+public class Salle {
+
+    private int idSalle;
+    private String nomSalle;
+    private String largeur;
+    private String longueur;
+    private String surface;
+    private String hauteur;
+    private int capacité;
+    private int tarifDemiJournée;
+    private int idLieu;
+}
